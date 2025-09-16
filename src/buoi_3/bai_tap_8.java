beddbe1f01a41c938e57511db25cf427d5f365ef
GiangQueMan <giangqueman@gmail.com>
2025-09-16 08:42:01 +0700

    sua logic bai tap

--- src/buoi_3/bai_tap_8.java
+++ src/buoi_3/bai_tap_8.java
@@ -1,12 +1,16 @@
 package buoi_3;
 
 public class bai_tap_8 {
+    // Dùng for-each để tính tổng các số chẵn trong mảng.
     public static void main(String[] args) {
+        // khai bao mang 1,2,3,4,5,6
         int[] numbers = {1,2,3,4,5,6};
-        int sum = numbers[0];
+        int sum = 0;
 
-        for(int num : numbers){
-            sum += num;
+        for(int num : numbers){ //khai báo biến num duyệt qua từng phần tử của mảng
+            if(num % 2 == 0){ //nếu là số chẵn thì cộng vào
+                sum += num;
+            }
         }
         System.out.println("tổng: " + sum);
     }
