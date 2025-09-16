88ff17b43a0d53fe9cde23d79f7aee5ef3bfae99
GiangQueMan <giangqueman@gmail.com>
2025-09-16 08:38:00 +0700

    sua logic bai tap

--- src/buoi_3/bai_tap_2.java
+++ src/buoi_3/bai_tap_2.java
@@ -1,13 +1,15 @@
 package buoi_3;
 
 public class bai_tap_2 {
+    // Dùng while để in ra các số chẵn trong khoảng 2 → 200
     public static void main(String[] args){
-        int i = 2;
-        while(i <= 200){
+        int i = 4;
+        while(i < 200){
             if(i % 2 == 0){
                 System.out.println("số sau đây là số chẵn: " + i);
             }
             i++;
         }
+        //cach 2: khong dung if, i += 2; Tăng mỗi lần lên 2 đơn vị để luôn là số chẳn
     }
 }
