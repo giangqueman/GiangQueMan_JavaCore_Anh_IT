2cee3040063d348fed0fd97b0c55eba0b5b69831
GiangQueMan <giangqueman@gmail.com>
2025-09-16 08:40:09 +0700

    sua kieu bien

--- src/buoi_3/bai_tap_4.java
+++ src/buoi_3/bai_tap_4.java
@@ -2,18 +2,23 @@ package buoi_3;
 
 import java.util.Scanner;
 
-public class bai_tap_4 {
+    public class bai_tap_4 {
+    // Nhập vào số nguyên n. Dùng for để tính n! (giai thừa của n).
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Nhập số nguyên từ bàn phím:
         System.out.print("nhap so nguyen: ");
         int n = input.nextInt();
 
-        int giaiThua = 1;
+        long giaiThua = 1;
+        // tính giai thừa bằng for
         for(int i = 1; i <= n; i++){
             giaiThua *= i;
         }
+
+        // in kết quả
         System.out.println("giai thừa của " + n + " là: " + giaiThua);
-
-
+        input.close();
     }
+
 }
