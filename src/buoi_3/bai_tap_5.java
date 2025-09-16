1a564cf7fbb278f48d9284ed0e113dfbcc11300e
GiangQueMan <giangqueman@gmail.com>
2025-09-16 08:41:29 +0700

    bo sung close scanner

--- src/buoi_3/bai_tap_5.java
+++ src/buoi_3/bai_tap_5.java
@@ -3,6 +3,7 @@ package buoi_3;
 import java.util.Scanner;
 
 public class bai_tap_5 {
+    // Nhập vào số n. Dùng for để in bảng cửu chương của n.
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("nhập số nguyên: ");
@@ -11,7 +12,7 @@ public class bai_tap_5 {
         for(int i = 1; i <= 10; i++){
             int a = n * i;
             System.out.println("bảng cửu chương " + n + ":" + n + "x" + i + "=" + a);
-
         }
+        input.close();
     }
 }
