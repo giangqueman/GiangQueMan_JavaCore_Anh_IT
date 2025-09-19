3bcc819ff02136cc6a6c9e258053676f0866ee9f
GiangQueMan <giangqueman@gmail.com>
2025-09-19 13:13:39 +0700

    Exercise session 5 bai 1

---
+++ src/buoi_5/HocSinh.java
@@ -0,0 +1,14 @@
+package buoi_5;
+
+public class HocSinh {
+
+    // thuộc tính: tên, tuổi, lớp
+    String name;
+    int age;
+    int grade;
+
+    // phương thức: học bài
+    void hocBai(){
+        System.out.println(name + " đang học bài");
+    }
+}
