de04241c1009179c03c65fc9ff80191c3b24a887
GiangQueMan <giangqueman@gmail.com>
2025-10-07 22:37:55 +0700

    update full project

--- src/ASM_quan_ly_garage_xe/Vehicle.java
+++ src/ASM_quan_ly_garage_xe/Vehicle.java
@@ -1,7 +1,8 @@
 package ASM_quan_ly_garage_xe;
-public class Vehicle{
 
-    private static int autoIncrementId = 0;
+abstract class Vehicle {
+    private static int autoIncrementId = 1;
+    private static int totalVehicle = 0;
     private int id;
     private String brand;
     private String model;
@@ -9,10 +10,9 @@ public class Vehicle{
     private double price;
     private String color;
     private String type;
-    private static int totalVehicle;
 
-    // phương thức
-    public Vehicle(String brand, String model, int year, double price, String color, String type){
+    // constructor
+    public Vehicle(String brand, String model, int year, double price, String color, String type) {
         this.id = autoIncrementId++;
         this.brand = brand;
         this.model = model;
@@ -20,45 +20,14 @@ public class Vehicle{
         this.price = price;
         this.color = color;
         this.type = type;
-        this.totalVehicle++;
+        totalVehicle++;
+
     }
 
-    //phương thức discount
-    public void applyDiscount(double discount){
-        price = price - (price * discount);
-    }
+    //
 
-    // phương thức toString
-    public String toString(){
-        return "Brand: " + brand + "Model: " + model + "Year: " + year + "Price: " + price + "Color: " + color;
-    }
-
-    //getter và setter
     public int getId(){
         return id;
-    }
-    public String getBrand(){
-        return brand;
-    }
-
-    public void setBrand(String brand){
-        this.brand = brand;
-    }
-
-    public String getModel(String model){
-        return model;
-    }
-
-    public void setModel(String model){
-        this.model = model;
-    }
-
-    public int getYear(){
-        return year;
-    }
-
-    public void setYear(int year){
-        this.year = year;
     }
     public double getPrice(){
         return price;
@@ -68,23 +37,35 @@ public class Vehicle{
         this.price = price;
     }
 
-    public String getColor(){
-        return color;
+    public String getBrand(){
+        return brand;
     }
 
-    public void setColor(String color){
-        this.color = color;
+    public void setBrand(String brand){
+        this.brand = brand;
     }
 
-    public String getType(){
-        return type;
+    public int getYear(){
+        return year;
     }
 
-    public void setType(String type){
-        this.type = type;
+    public void setYear(int year){
+        this.year = year;
     }
 
     public static int getTotalVehicle(){
         return totalVehicle;
     }
+
+    public static void decreaseTotalVehicles() {
+        if (totalVehicle > 0) totalVehicle--;
+    }
+
+    // phương thức
+    public void displayInfo(){
+        System.out.println("ID: " + id + " | Brand: " + brand + " | Model: " + model + " | Year: " + year + " | Price: " + price + " | Color: " + color + " | Type: " + type);
+    }
+
+    public abstract void applyDiscount();
+
 }
